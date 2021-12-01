279b4381db567157262f376a561c047957d30c1c
s20193068 <s20193068@HCS54M115.stdlan.hcs.local>
2021-12-01 11:39:29 +0900

    git連携

---
+++ src/test/java/jp/ac/hcs/s3a310/HcsTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.hcs.s3a310;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcsTubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
