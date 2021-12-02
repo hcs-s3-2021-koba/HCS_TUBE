29ea374b20c97c2dbd6bd4800a2a7bcfb2151ea9
s20193068 <s20193068@HCS54M115.stdlan.hcs.local>
2021-12-02 11:53:40 +0900

    起動

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -27,11 +27,9 @@ CREATE TABLE IF NOT EXISTS comments (
 	user_id VARCHAR(254),
 	movie_id INT(254),
 	comment VARCHAR(254) NOT NULL,
-	PRIMARY KEY (post_time),
-	PRIMARY KEY (user_id),
-	PRIMARY KEY (movie_id),
+	PRIMARY KEY (post_time,user_id,movie_id),
 	FOREIGN KEY (user_id) REFERENCES users(user_id),
-	FOREIGN KEY (movie_id) REFERENCES users(movie_id)
+	FOREIGN KEY (movie_id) REFERENCES movies(movie_id)
 );
 
 /* ライブマスタ */
