2a18a27f7f7e64bda4688ea68e3708accfeb5e3f
s20193068 <s20193068@HCS54M115.stdlan.hcs.local>
2021-12-08 10:24:23 +0900

    動画視聴画面に遷移(仮)

--- src/main/java/jp/ac/hcs/s3a310/SecurityConfig.java
+++ src/main/java/jp/ac/hcs/s3a310/SecurityConfig.java
@@ -40,6 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/login").permitAll() //ログインプロセスページは直リンクOK
 				.antMatchers("/top").permitAll() // ログインページは直リンクOK
 				.antMatchers("/signup").permitAll() // 新規ユーザー登録画面は直リンクOK
+				.antMatchers("/watch_video").permitAll() // 動画視聴画面は直リンクOK
+				.antMatchers("/watch_live").permitAll() // ライブ視聴画面は直リンクOK
 				.antMatchers("/user/**").hasAuthority("")// ユーザ管理機能は管理権限ユーザに許可
 				.anyRequest().authenticated(); // それ以外は直リンク禁止
 		//ログイン処理
--- src/main/java/jp/ac/hcs/s3a310/movie/MovieController.java
+++ src/main/java/jp/ac/hcs/s3a310/movie/MovieController.java
@@ -1,5 +1,14 @@
 package jp.ac.hcs.s3a310.movie;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MovieController {
 
+	@GetMapping("/watcthMovie")
+	public String watchMovie() {
+		return "movie/watch_video";
+
+	}
 }
