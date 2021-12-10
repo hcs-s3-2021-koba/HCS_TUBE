39973350551d6d3e1159d50c7d9813fd81359758
s20193068 <s20193068@HCS54M115.stdlan.hcs.local>
2021-12-10 11:51:45 +0900

    TOP画面動画表示(途中)

--- src/main/java/jp/ac/hcs/s3a310/movie/MovieRepository.java
+++ src/main/java/jp/ac/hcs/s3a310/movie/MovieRepository.java
@@ -1,9 +1,63 @@
 package jp.ac.hcs.s3a310.movie;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MovieRepository {
+	/** SQL 全件取得(投稿日時の降順) */
+	private static final String SQL_SELECT_ALL_MOVIE = "SELECT * FROM movies ORDER BY post_time DESC";
+
+
+	@Autowired
+	private JdbcTemplate jdbc;
+
+	public MovieEntity selectAll() {
+		List<Map<String, Object>> resultList = jdbc.queryForList(SQL_SELECT_ALL_MOVIE);
+		MovieEntity movieEntity = mappingSelectResult(resultList);
+		return movieEntity;
+	}
+
+	private MovieEntity mappingSelectResult(List<Map<String, Object>> resultList) {
+		MovieEntity entity = new MovieEntity();
+
+		for (Map<String, Object> map : resultList) {
+			MovieData data = new MovieData();
+			data.setMovie_id((int)map.get("movie_id"));
+			data.setUser_id((String) map.get("user_id"));
+			data.setPost_time(ChangeDate(map.get("post_time")));
+			data.setMovie_title((String) map.get("movie_title"));
+			data.setMovie_detail((String)map.get("movie_detail"));
+			data.setFile_name((String)map.get("file_name"));
+			data.setThumbnail((String)map.get("thumbnail"));;
+			entity.getMovielist().add(data);
+		}
+		return entity;
+	}
+
+	/**
+	 * Date型変換
+	 * @param object 取得した申請内容
+	 * @return date2
+	 */
+	private String ChangeDate(Object object) {
+
+		String date2;
+
+		if(!(object == null)) {
+			SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+			date2 = format.format(object);
+		} else {
+			date2 = "";
+		}
+		return date2;
+	}
+
 
 
 }
--- src/main/java/jp/ac/hcs/s3a310/movie/MovieService.java
+++ src/main/java/jp/ac/hcs/s3a310/movie/MovieService.java
@@ -1,5 +1,19 @@
 package jp.ac.hcs.s3a310.movie;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MovieService {
+
+	@Autowired MovieRepository movieRepository;
+
+	/**
+	 * 動画情報を全件取得する
+	 * @return MovieEntity
+	 */
+	public MovieEntity selectAll() {
+		return movieRepository.selectAll();
+	}
 
 }
