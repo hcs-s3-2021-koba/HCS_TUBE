27c42eaa0515c964683f79b6cc9c242652825876
s20193015 <s20193015@HCS54M121.stdlan.hcs.local>
2021-12-13 11:53:54 +0900

    修正

--- src/main/java/jp/ac/hcs/s3a310/comment/CommentService.java
+++ src/main/java/jp/ac/hcs/s3a310/comment/CommentService.java
@@ -2,7 +2,8 @@ package jp.ac.hcs.s3a310.comment;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-
+import org.springframework.stereotype.Service;
+@Service
 public class CommentService {
 	@Autowired
 	CommentRepository commentRepository;
