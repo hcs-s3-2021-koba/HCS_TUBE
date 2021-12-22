ff8783e0f175220b6d0b155701ba97781b420df6
s20193089 <s20193089@HCS54M101.stdlan.hcs.local>
2021-12-22 11:26:51 +0900

    liveテスト用遷移

--- src/main/java/jp/ac/hcs/s3a310/live/LiveController.java
+++ src/main/java/jp/ac/hcs/s3a310/live/LiveController.java
@@ -1,5 +1,15 @@
 package jp.ac.hcs.s3a310.live;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class LiveController {
+
+	@RequestMapping("/liveMovie")
+	public String getLogin(Model model) {
+		return "movie/live_control";
+	}
 
 }
