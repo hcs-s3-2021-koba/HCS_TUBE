00cddf092a341ff4058d526ab3911b3d446f1477
s20193068 <s20193068@HCS54M115.stdlan.hcs.local>
2022-02-02 09:15:38 +0900

    pom.xml修正(ojdbc8エラー状態)

--- src/main/java/jp/ac/hcs/s3a310/HcsTubeApplication.java
+++ src/main/java/jp/ac/hcs/s3a310/HcsTubeApplication.java
@@ -2,12 +2,15 @@ package jp.ac.hcs.s3a310;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 @SpringBootApplication
-public class HcsTubeApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(HcsTubeApplication.class, args);
-	}
-
+public class HcsTubeApplication extends SpringBootServletInitializer{
+  public static void main(String[] args) {
+    SpringApplication.run(HcsTubeApplication.class, args);
+  }
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+      return builder.sources(HcsTubeApplication.class);
+    }
 }
--- src/main/java/jp/ac/hcs/s3a310/main/LoginController.java
+++ src/main/java/jp/ac/hcs/s3a310/main/LoginController.java
@@ -1,8 +1,5 @@
 package jp.ac.hcs.s3a310.main;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 * ログインに関する機能・画面を制御する
 */
 @Controller
-@SpringBootApplication
-public class LoginController extends SpringBootServletInitializer {
+public class LoginController {
 	/**
 	 * ログイン画面を表示する
 	 * @param model ログイン情報を受け渡す
@@ -22,9 +18,4 @@ public class LoginController extends SpringBootServletInitializer {
 	public String getLogin(Model model) {
 		return "user/login";
 	}
-
-	@Override
-	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-	      return builder.sources(LoginController.class);
-	    }
 }
