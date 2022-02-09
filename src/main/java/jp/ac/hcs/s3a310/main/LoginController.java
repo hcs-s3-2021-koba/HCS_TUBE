aabedcdc3969586548d4e73f5e9846ca5ec94f52
s20193006 <s20193006@HCS54M110.stdlan.hcs.local>
2022-02-09 11:47:41 +0900

    ログイン処理

--- src/main/java/jp/ac/hcs/s3a310/main/LoginController.java
+++ src/main/java/jp/ac/hcs/s3a310/main/LoginController.java
@@ -1,14 +1,25 @@
 package jp.ac.hcs.s3a310.main;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jp.ac.hcs.s3a310.SecurityConfig;
+import jp.ac.hcs.s3a310.user.UserService;
 
 /**
 * ログインに関する機能・画面を制御する
 */
 @Controller
 public class LoginController {
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	SecurityConfig securityConfig;
+
 	/**
 	 * ログイン画面を表示する
 	 * @param model ログイン情報を受け渡す
@@ -19,7 +30,7 @@ public class LoginController {
 		return "user/login";
 	}
 
-	
+
 }
 
 
